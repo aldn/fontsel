69901ee06765642b99efa2487925af6a0104d7a2
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2016-08-21 20:59:55 +0300

    initial

---
+++ fontsel.cpp
@@ -0,0 +1,32 @@
+/*
+ * Browser for installed fonts with a simple preview.
+ *
+ * (c) 2016 Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <QApplication>
+#include <QFontDialog>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    const QFont initialFont("Sans Serif", 18, QFont::Normal);
+    QFontDialog fontDialog(initialFont);
+    fontDialog.resize(600, 600);
+    fontDialog.setOption(QFontDialog::NoButtons);
+    return fontDialog.exec();
+}
